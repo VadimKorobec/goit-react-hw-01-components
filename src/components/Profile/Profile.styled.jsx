f1a3b0aa16889276039fb7c20ba7d55e369c92cb
VadimKorobec <_s_e_t__@ukr.net>
2023-01-04 17:33:43 +0200

    Update Profile.styled.jsx

--- src/components/Profile/Profile.styled.jsx
+++ src/components/Profile/Profile.styled.jsx
@@ -13,7 +13,7 @@ export const Description = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 30px 0;
+  padding: 30px;
 `;
 
 export const Avatar = styled.img`
