f20ae1d233d9f2d9edf0ce93881e2ed0be03894e
VadimKorobec <_s_e_t__@ukr.net>
2023-01-04 17:09:14 +0200

    Update TransactionHistory.styled.jsx

---
+++ src/components/TransactionHistory/TransactionHistory.styled.jsx
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const Table = styled.table`
+  margin: 20px auto 0px auto;
+  width: 600px;
+
+  border-collapse: collapse;
+  border-spacing: 0;
+  border-radius: 5px;
+  background-color: #fafafa;
+  box-shadow: 2px 3px 14px 2px rgba(0, 0, 0, 0.41);
+  overflow: hidden;
+`;
+
+export const TableHead = styled.tr`
+  height: 40px;
+  background-color: #00bfff;
+  /* text-align: center; */
+`;
