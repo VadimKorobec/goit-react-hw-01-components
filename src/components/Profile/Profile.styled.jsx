a2750b15aaafb750c9c10bf65f048f1263f437db
VadimKorobec <_s_e_t__@ukr.net>
2023-01-04 15:09:18 +0200

    Update Profile.styled.jsx

---
+++ src/components/Profile/Profile.styled.jsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const Card = styled.div`
+  margin: 0 auto;
+  width: 300px;
+  background-color: #fafafa;
+  border-radius: 5px;
+  box-shadow: 2px 3px 14px 2px rgba(0, 0, 0, 0.41);
+  overflow: hidden;
+`;
